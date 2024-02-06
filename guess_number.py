2ca697eae15dd4b0e219023d2a7383ccaff8b52e
Alexander Fiodorov <alexfiodorov02@gmail.com>
2024-02-06 06:11:25 -0600

    Added code to cap total number of guesses before showing user answer

--- guess_number.py
+++ guess_number.py
@@ -1,7 +1,8 @@
 number = 10
+max_guesses = 5
 
 print("I'm thinking of a number...")
-while True:
+for i in range(max_guesses, 0, -1):
     guess = input("What number am I thinking of? ")
     if guess == 'q':
         print(f"You quit! The number was {number}.")
@@ -9,5 +10,9 @@ while True:
     elif int(guess) == number:
         print("Congratulations! You guessed the right number.")
         break
+    elif int(guess) > number:
+        print(f"Your guess is too high. Try again. You have {i-1} guesses left.")
     else:
-        print("Incorrect guess. Try again.")
+        print(f"Your guess is too low. Try again. You have {i-1} guesses left.")
+else:
+    print(f"Sorry! You've run out of guesses. The number was {number}.")
